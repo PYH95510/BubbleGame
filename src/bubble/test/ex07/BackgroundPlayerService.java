e00b884a3dfc06bfc03b478fa0e9227d5a90eb76
Yohan <pyh95510@gmail.com>
2022-12-01 13:48:58 -0500

    wall crash works, it doesn't go outside the wall

---
+++ src/bubble/test/ex07/BackgroundPlayerService.java
@@ -0,0 +1,56 @@
+package bubble.test.ex07;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+// Main thread is busy
+// this is going to be new thread
+public class BackgroundPlayerService implements Runnable{
+
+	private BufferedImage image;
+	private Player player;
+	
+	public BackgroundPlayerService(Player player) {
+		this.player = player;
+		try {
+			image = ImageIO.read(new File("image/backgroundMapService.png"));
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	@Override
+	public void run() {
+		while(true) {
+			// checking colors
+			Color leftColor = new Color(image.getRGB(player.getX() - 10, player.getY() + 25));
+			Color rightColor = new Color(image.getRGB(player.getX() + 50 + 15, player.getY() + 25));
+			//System.out.println("leftColor : "+leftColor);
+			//System.out.println("rightColor : "+rightColor);
+			
+			if(leftColor.getRed() == 255 && leftColor.getGreen() == 0 && leftColor.getBlue() == 0) {
+				System.out.println("collide with left wall");
+				player.setLeftWallCrash(true);
+				player.setLeft(false);
+			}else if(rightColor.getRed() == 255 && rightColor.getGreen() == 0 && rightColor.getBlue() == 0) {
+				System.out.println("collide with right wall");
+				player.setRightWallCrash(true);
+				player.setRight(false);
+			}else {
+				player.setLeftWallCrash(false);
+				player.setRightWallCrash(false);
+			}
+			
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
---
+++ src/bubble/test/ex07/bubbleFrame.java
@@ -0,0 +1,85 @@
+package bubble.test.ex07;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class bubbleFrame extends JFrame{
+	
+		private JLabel backgroundMap;
+		private Player player;
+	
+		public bubbleFrame() {
+			initObject();
+			initSetting();
+			initListener();
+			setVisible(true);
+		}
+	
+		private void initObject() {
+			backgroundMap = new JLabel(new ImageIcon("image/backgroundMap.png"));
+			setContentPane(backgroundMap);
+			
+			player = new Player();
+			add(player);
+
+		}
+		
+		private void initSetting() {
+			setSize(1000,640);
+			setLayout(null); //absolute, we could freely draw
+			setLocationRelativeTo(null); //locate to middle
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //terminate JVM
+		}
+		
+		private void initListener() {
+			addKeyListener(new KeyAdapter() {
+
+				
+				@Override
+				public void keyPressed(KeyEvent e) {
+					// System.out.println(e.getKeyCode());
+
+					switch (e.getKeyCode()) {
+					case KeyEvent.VK_LEFT:
+						if (!player.isLeft()&& !player.isLeftWallCrash()) {
+							player.left();
+						}
+						break;
+					case KeyEvent.VK_RIGHT:
+						if (!player.isRight() && !player.isRightWallCrash()) {
+							player.right();
+						}
+						break;
+					case KeyEvent.VK_UP:
+						if(!player.isUp() && !player.isDown()) { //if it is not up and down, it could jump
+							player.up();
+						}
+						break;
+					}
+				}
+
+				
+				@Override
+				public void keyReleased(KeyEvent e) {
+					switch (e.getKeyCode()) {
+					case KeyEvent.VK_LEFT:
+						player.setLeft(false);
+						break;
+					case KeyEvent.VK_RIGHT:
+						player.setRight(false);
+						break;
+					}
+				}
+
+			});
+		}
+
+		public static void main(String[] args) {
+			new bubbleFrame();
+		}
+	}
